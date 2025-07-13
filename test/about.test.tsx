01057633f583b770d2877d913aac0bbb4502a3e6
Bronina A.O <bronina04@mail.ru>
2025-07-13 20:10:50 +0300

    fix react test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { About } from '../src/client/pages/About';
+import React from 'react';
 
 describe('About', () => {
   it('Should render author name', () => {
